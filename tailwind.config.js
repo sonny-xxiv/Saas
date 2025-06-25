6d3f51fe7f43025642e02cae0d0de465bce453f5
sonny-xxiv <oludelesamson3@gmail.com>
2025-06-25 15:24:06 +0100

    Reweb commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          instrument: ['"Instrument Sans"', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
